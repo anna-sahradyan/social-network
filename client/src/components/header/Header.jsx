74e3a1ab48f9fc619209e7c026dd867457499c4e
anna-sahradyan <a.sahradyan72@gmail.com>
2022-12-23 20:52:48 +0400

    add pages and components directory

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Left, Right, Logo, Link, Button} from "./headerStyle";
+
+
+const Header = () => {
+    const isAuth = true;
+    return (
+        <>
+            <Container>
+                <Left>
+                    <Link> <Logo>ANNASOCIAL</Logo></Link>
+
+                </Left>
+
+                <Right>
+                    {!isAuth?(<>
+                        <Link href={"/posts/create"}>
+                            <Button>Create Post</Button>
+                        </Link>
+                        <Link>
+                            <Button>Log out</Button>
+                        </Link>
+                    </>):(<>
+                        <Link href={"/login"}>
+                            <Button>Create an account</Button>
+                        </Link>
+                        <Link href={"/register"}>
+                            <Button>Log in</Button>
+                        </Link>
+                    </>)
+
+                    }
+
+                </Right>
+
+            </Container>
+        </>
+    );
+};
+
+export default Header;
---
+++ client/src/components/header/headerStyle.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: #2D7ECD;
+  position: sticky;
+  display: flex;
+  align-items: center;
+
+`;
+export const Left = styled.div`
+  display: flex;
+  flex: 4;
+  margin-left: 15px;
+
+
+`;
+
+export const Right = styled.div`
+  display: flex;
+  flex: 8;
+  justify-content: end;
+
+
+
+`;
+export const Logo = styled.h2`
+  color: white;
+
+`;
+export const Link = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+
+`;
+export const Button = styled.button`
+  width: 150px;
+  margin: 6px;
+  border: none;
+  padding: 5px 20px;
+  border-radius: 5px;
+  color: teal;
+  cursor: pointer;
+  
+  
+`;
