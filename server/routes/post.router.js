a9193d4cc8adc06bca61c8d28f8c7c9e65ad2915
anna-sahradyan <a.sahradyan72@gmail.com>
2022-12-23 12:31:41 +0400

    add client app

--- server/routes/post.router.js
+++ server/routes/post.router.js
@@ -2,14 +2,15 @@ import express from "express";
 import {create, getAll, getOne, remove, update} from "../controllers/postController.js";
 import {postCreateValidator} from "../validations/index.js";
 import checkAuth from "../utils/checkAuth.js";
+import handleValidationErrors from "../utils/handleValidationErrors.js";
 
 
 const router = express.Router();
 router.get(`/`, getAll);
 router.get(`/:id`, getOne);
-router.post(`/`, checkAuth, postCreateValidator, create);
+router.post(`/`, checkAuth, postCreateValidator,handleValidationErrors, create);
 router.delete(`/:id`, checkAuth, remove);
-router.patch(`/:id`, checkAuth,postCreateValidator, update);
+router.patch(`/:id`, checkAuth,postCreateValidator,handleValidationErrors, update);
 
 
 
